376a3ba5a43038301f8fad6298211b8d992acb59
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2021-08-04 17:55:04 -0400

    Moved FlowLayoutPanel from its own class to the main class

---
+++ RoguelikeCompanion/RoguelikeCompanion/IndividualWeaponForm.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RoguelikeCompanion
+{
+    public partial class IndividualWeaponForm : Form
+    {
+        // Class members;
+        PictureBox imagePB;
+        WeaponObjectForm obj;
+
+        public IndividualWeaponForm(Image img, string name, string dps, string reloadTime, string sellPrice, string gunType)
+        {
+            InitializeComponent();
+            this.imagePB = createPictureBox(img);
+            this.obj = new WeaponObjectForm(name, dps, reloadTime, sellPrice, gunType);
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // Remove borders
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+
+            // Create an image object in the top left corner
+            imagePB.Location = new System.Drawing.Point(0, 0);
+
+            // Put DataGridView below image
+            int imageOffset = imagePB.Height;
+            DataGridView data = obj.weaponDataGrid();
+            data.Location = new System.Drawing.Point(0, imageOffset);
+
+            // Add image and grid to form
+            this.Controls.Add(imagePB);
+            this.Controls.Add(data);
+
+            // Set form size
+            int width = data.Width;
+            int height = imageOffset + data.Height;
+            this.Size = new Size(width, height);
+
+        }
+
+        /*
+         */
+        public PictureBox createPictureBox(Image img)
+        {
+            PictureBox imageBox = new PictureBox();
+            Bitmap bm = new Bitmap(img);
+            bm = ScaleImage(bm, 100, 200);
+            imageBox.Height = 100;
+            imageBox.Width = 200;
+            imageBox.Image = bm;
+
+            return imageBox;
+        }
+
+        /*
+         */
+        public static Bitmap ScaleImage(Bitmap img, int height, int width)
+        {
+            if (img == null || height <= 0 || width <= 0)
+            {
+                return null;
+            }
+            int newWidth = (img.Width * height) / (img.Height);
+            int newHeight = (img.Height * width) / (img.Width);
+            int x = 0;
+            int y = 0;
+
+            Bitmap bmp = new Bitmap(width, height);
+            Graphics g = Graphics.FromImage(bmp);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear;
+
+            if (newWidth > width)
+            {
+                // New height
+                x = (bmp.Width - width) / 2;
+                y = (bmp.Height - newHeight) / 2;
+                g.DrawImage(img, x, y, width, newHeight);
+            }
+            else
+            {
+                // New width
+                x = (bmp.Width / 2) - (newWidth / 2);
+                y = (bmp.Height / 2) - (height / 2);
+                g.DrawImage(img, x, y, newWidth, height);
+            }
+
+            return bmp;
+        }
+
+    }
+}
---
+++ RoguelikeCompanion/RoguelikeCompanion/MainWindowForm.cs
@@ -0,0 +1,80 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RoguelikeCompanion
+{
+    public partial class MainWindowForm : Form
+    {
+        double[,] borderData = BorderClass.readBorderData();
+        NeuralNetwork nn = new NeuralNetwork();
+        FlowLayoutPanel dynamicFlowLayoutPanel;
+
+        public MainWindowForm()
+        {
+            InitializeComponent();
+            this.IsMdiContainer = true;
+            this.WindowState = FormWindowState.Maximized;
+
+            // Create all weapons form
+            //AllWeaponsForm allWeapons = new AllWeaponsForm(this.Width * 65 / 100, this.Height);
+            //allWeapons.MdiParent = this;
+            //allWeapons.Location = new Point(0, 0);
+            //allWeapons.Show();
+
+            // Create container for all weapons
+            dynamicFlowLayoutPanel = new FlowLayoutPanel();
+            dynamicFlowLayoutPanel.Name = "weaponFlowLayoutPanel";
+            dynamicFlowLayoutPanel.Width = this.Width * 65 / 100;
+            dynamicFlowLayoutPanel.Height = this.Height;
+            dynamicFlowLayoutPanel.TabIndex = 0;
+            dynamicFlowLayoutPanel.FlowDirection = FlowDirection.LeftToRight;
+            this.Controls.Add(dynamicFlowLayoutPanel);
+            Image test = Image.FromFile("C:/Users/Grant/Desktop/Java_Rouge_Like_App/screenshots/temp_0.jpg");
+            IndividualWeaponForm formChild = new IndividualWeaponForm(test, "Crossbow", "33.2", "0.3 seconds", "21", "Automatic");
+            formChild.MdiParent = this;
+            dynamicFlowLayoutPanel.Controls.Add(formChild);
+            formChild.Show();
+
+
+            // For testing
+            //Image test = Image.FromFile("C:/Users/Grant/Desktop/Java_Rouge_Like_App/screenshots/temp_0.jpg");
+            //IndividualWeaponForm formChild = new IndividualWeaponForm(test, "Crossbow", "33.2", "0.3 seconds", "21", "Automatic");
+            //formChild.MdiParent = this;
+            //formChild.Show();
+        }
+
+        private void capture_Click(object sender, EventArgs e)
+        {
+            WeaponObjectForm test = new WeaponObjectForm("Crossbow", "33.2", "0.3 seconds", "21", "Automatic");
+            //test.weaponListBox();
+            this.Controls.Add(test.weaponDataGrid());
+
+            /*
+            */
+            //NeuralNetwork nn = new NeuralNetwork();
+            System.Threading.Thread.Sleep(4000);
+            
+            // Obtain bitmaps
+            Bitmap initialImage = ScreenImgCapture.bitmapScreenCapture();
+            Bitmap notificationBox = ScreenImgCapture.cropBitMap(initialImage, 567, 425, 767, 77);
+            Bitmap borderNotificationBox = ScreenImgCapture.cropBitMap(notificationBox, 25, notificationBox.Width - 25, 0, notificationBox.Height);
+
+            // Check for notification box
+            int borderClass = BorderClass.predictIsBorder(borderNotificationBox, borderData);
+            if(borderClass != 0 && borderClass != 4)
+            {
+                string guess = nn.newImagePrediction(notificationBox);
+                int temp = 5;
+
+            }
+        }
+    }
+}

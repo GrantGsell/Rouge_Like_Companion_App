333f8505e509eb621fc1a8b65c8c8054dddc8a24
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2021-08-08 13:59:51 -0400

    Fixed synergy image method

--- RoguelikeCompanion/RoguelikeCompanion/ObjectInformation.cs
+++ RoguelikeCompanion/RoguelikeCompanion/ObjectInformation.cs
@@ -117,7 +117,7 @@ namespace RoguelikeCompanion
 
         /*
          */
-        public Tuple<string, string, string, Bitmap> obtainItemStats(string itemName)
+        public static Tuple<string, string, string, Bitmap> obtainItemStats(string itemName)
         {
             try
             {
@@ -133,7 +133,7 @@ namespace RoguelikeCompanion
                 MySqlCommand MyCommand;
 
                 // Write and execute query
-                query = "SELECT object_id, object_name, item_type, effect, img" +
+                query = "SELECT object_id, object_name, item_type, effect, img " +
                         "FROM objects " +
                         "LEFT JOIN item_stats USING (object_id)  " +
                         "WHERE object_name = \'" + itemName + "\'";
@@ -146,7 +146,7 @@ namespace RoguelikeCompanion
                 string objectName = (string)table.Rows[0][1];
                 string itemType = (string)table.Rows[0][2];
                 string effect = (string)table.Rows[0][3];
-                byte[] itemImageRaw = (byte[])table.Rows[0][3];
+                byte[] itemImageRaw = (byte[])table.Rows[0][4];
 
                 // Transform image byte data into bitmap
                 MemoryStream ms = new MemoryStream(itemImageRaw);
@@ -163,6 +163,104 @@ namespace RoguelikeCompanion
                 Console.WriteLine(e.Message);
             }
             return null;
+        }
+
+
+        /*
+         */
+        public static List<Tuple<Bitmap, string>> obtainSynergyStats(string objectName)
+        {
+            List<Tuple<Bitmap, string>> results = new List<Tuple<Bitmap, string>>();
+            try
+            {
+                // Create connector and reader objects
+                MySqlConnection MyConnection = null;
+                MySqlDataReader MyReader = null;
+
+                // Create the SQL connection
+                MyConnection = new MySqlConnection(SQLInfo.getLogin());
+                MyConnection.Open();
+
+                // Create a query string and command
+                String query;
+                MySqlCommand MyCommand;
+
+                // Write and execute query
+                query = "SELECT " +
+                        "object_id, object_name, is_gun, " +
+                        "synergy_object_name, synergy_object_text, synergy_is_gun " +
+                        "FROM objects " +
+                        "INNER JOIN synergies USING (object_id) " +
+                        "WHERE object_name = \'" + objectName + "\'";
+                MyCommand = new MySqlCommand(query, MyConnection);
+
+                // Obtain all synergy data
+                MyReader = MyCommand.ExecuteReader();
+
+                // Obtain data for each synergy object
+                while (MyReader.Read())
+                {
+                    // Obtain synergy object name and is_gun
+                    string synergyObjectName = MyReader.GetString(3);
+                    bool synergy_is_gun = MyReader.GetBoolean(5);
+
+                    // Create a second connector and reader objects
+                    MySqlConnection MyConnection2 = null;
+                    MySqlCommand MyCommand2;
+
+                    // Create the SQL connection
+                    MyConnection2 = new MySqlConnection(SQLInfo.getLogin());
+                    MyConnection2.Open();
+
+                    // Obtain synergy object image
+                    byte[] itemImageRaw;
+                    if (synergy_is_gun)
+                    {
+                        query = "SELECT " +
+                                "object_id, object_name, pic " +
+                                "FROM objects " +
+                                "LEFT JOIN gun_stats ON object_id = gun_id " +
+                                "WHERE object_name = \'" + synergyObjectName + "\'";
+                        MyCommand2 = new MySqlCommand(query, MyConnection2);
+                        MySqlDataAdapter da = new MySqlDataAdapter(MyCommand2);
+                        DataTable table = new DataTable();
+                        da.Fill(table);
+                        itemImageRaw = (byte[])table.Rows[0][2];
+                    }
+                    else
+                    {
+                        query = "SELECT " +
+                                "object_id, object_name, img " +
+                                "FROM objects " +
+                                "LEFT JOIN item_stats USING (object_id) " +
+                                "WHERE object_name = \'" + synergyObjectName + "\'";
+                        MyCommand2 = new MySqlCommand(query, MyConnection2);
+                        MySqlDataAdapter da = new MySqlDataAdapter(MyCommand2);
+                        DataTable table = new DataTable();
+                        da.Fill(table);
+                        itemImageRaw = (byte[])table.Rows[0][2];
+                    }
+
+                    // Transform image byte data into bitmap
+                    MemoryStream ms = new MemoryStream(itemImageRaw);
+                    Bitmap itemImage = new Bitmap(ms);
+
+                    // Close the second connection
+                    MyConnection2.Close();
+
+                    // Add data to results list
+                    results.Add(Tuple.Create(itemImage, objectName));
+                }
+
+                // Close connection
+                MyConnection.Close();
+            }
+            catch (MySqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return results;
         }
     }
 }

57f4984c159f38b80c3985cfd7da141deede7d44
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2021-08-22 20:24:02 -0400

    Added form for shrines

---
+++ RoguelikeCompanion/RoguelikeCompanion/Shrines.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RoguelikeCompanion
+{
+    public partial class Shrines : Form
+    {
+        public Shrines(int width, int height)
+        {
+            InitializeComponent();
+            this.Size = new Size(width, height);
+        }
+
+        private void Shrines_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /*
+         */
+        public void getShrineData()
+        {
+
+        }
+    }
+}

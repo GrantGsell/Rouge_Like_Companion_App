f7d47fa11de0e0baa9fbe33848f97e3d03cc899f
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2021-08-23 20:52:26 -0400

    Added method to dynamically create buttons for each shrine with their images as backgrounds

--- RoguelikeCompanion/RoguelikeCompanion/Shrines.cs
+++ RoguelikeCompanion/RoguelikeCompanion/Shrines.cs
@@ -12,6 +12,9 @@ namespace RoguelikeCompanion
 {
     public partial class Shrines : Form
     {
+        List<Tuple<string, Bitmap, string>> shrineData;
+        List<Tuple<string, string>> goodDiceEffects;
+        List<Tuple<string, string>> badDiceEffects;
         public Shrines(int width, int height)
         {
             InitializeComponent();
@@ -20,7 +23,8 @@ namespace RoguelikeCompanion
 
         private void Shrines_Load(object sender, EventArgs e)
         {
-
+            getShrineData();
+            createButtons();
         }
 
 
@@ -28,7 +32,37 @@ namespace RoguelikeCompanion
          */
         public void getShrineData()
         {
+            shrineData = ObjectInformation.getShrineData();
+            goodDiceEffects = ObjectInformation.getDiceEffects(true);
+            badDiceEffects = ObjectInformation.getDiceEffects(false);
+        }
 
+
+        /*
+         */
+        public void createButtons()
+        {
+            // Obtain the size of each button
+            int buttonWidth = this.Size.Width / 5;
+            int buttonHeight = this.Size.Height / 3;
+
+            // Create, add backgrond to buttons
+            int row = 0;
+            int col = 0;
+            for(int i = 0; i < shrineData.Count; i++)
+            {
+                string newButtonName = "btn" + shrineData[i].Item1;
+                Button newButton = new Button();
+                newButton.Size = new Size(buttonWidth, buttonHeight);
+                newButton.Name = newButtonName;
+                newButton.BackgroundImage = shrineData[i].Item2;
+                newButton.BackgroundImageLayout = ImageLayout.Stretch;
+                newButton.Location = new Point(buttonWidth * col, buttonHeight * row);
+                this.Controls.Add(newButton);
+                col++;
+                col = (col > 4) ? 0 : col;
+                row = (col == 0) ? row + 1 : row;
+            }
         }
     }
 }

5da898485cc969d3bc7be5d6a5b34b81591ce261
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2021-08-30 22:04:12 -0400

    Added file for NN Unit Tests

---
+++ RoguelikeCompanion/TestRoguelikeCompanion/TestNeuralNetwork.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestRoguelikeCompanion
+{
+    [TestClass]
+    public class TestNeuralNetwork
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

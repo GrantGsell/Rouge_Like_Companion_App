95c9bb4aafa4faa7e1d212fcf32ff73d3b88c078
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2022-03-01 20:33:06 -0500

    Added method to display extracted characters

--- RoguelikeCompanion/NeuralNetworkVisualization/NN_Visualization.cs
+++ RoguelikeCompanion/NeuralNetworkVisualization/NN_Visualization.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -18,15 +19,22 @@ namespace NeuralNetworkVisualization
         NeuralNetwork nn = new NeuralNetwork();
         string textBoxPathName = @"C:\Users\Grant\Desktop\Java_Rouge_Like_App\screenshots\temp_1.jpg";
 
+        /*
+         */
         public NN_Visualization()
         {
             InitializeComponent();
             Bitmap newImage = convertToBitmap(textBoxPathName);
             showExtractedTextBox(newImage);
             showCurrentSlidingWindow(newImage);
+            string guess = nn.newImagePrediction(newImage);
+            imageIsolation(newImage);
+            int test = 5;
         }
 
 
+        /*
+         */
         public void showExtractedTextBox(Bitmap textBoxImage)
         {
             // Set image and fit to size
@@ -34,6 +42,9 @@ namespace NeuralNetworkVisualization
             extractedTextBox.SizeMode = PictureBoxSizeMode.StretchImage;
         }
 
+
+        /*
+         */
         public void showCurrentSlidingWindow(Bitmap slidingWindow)
         {
             // Set image and fit to size
@@ -42,18 +53,44 @@ namespace NeuralNetworkVisualization
         }
 
 
+        /*
+         */
         public Bitmap convertToBitmap(string fileName)
         {
             Bitmap bitmap;
-            using (Stream bmpStream = System.IO.File.Open(fileName, System.IO.FileMode.Open))
+            using (Stream bmpStream = File.Open(fileName, FileMode.Open))
             {
                 Image image = Image.FromStream(bmpStream);
-
                 bitmap = new Bitmap(image);
-
             }
             return bitmap;
         }
 
+
+        public void imageIsolation(Bitmap newImage)
+        {
+            // Timer
+            System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+            int delayTime = 2000;
+            myTimer.Interval = delayTime;
+
+            // Sliding window dimensions and step size
+            int swHeight = 18, swWidth = 15, swDelta = 5;
+
+            // Obtain indicies for character separation
+            List<int> charSeparationIndicies;
+            Bitmap isolatedTextImage;
+            (charSeparationIndicies, isolatedTextImage) = CharacterSegmentation.characterSegmentation(newImage);
+
+            showExtractedTextBox(isolatedTextImage);
+            for(int i = 0; i < isolatedTextImage.Width - swWidth + swDelta; i += swDelta)
+            {
+                //Bitmap test = ScreenImgCapture.cropBitMap(isolatedTextImage, charSeparationIndicies[0], swWidth, 0, swHeight);
+                Bitmap test = ScreenImgCapture.cropBitMap(isolatedTextImage, i, swWidth, 0, swHeight);
+                showCurrentSlidingWindow(test);
+            }
+           
+        }
+                                     
     }
 }

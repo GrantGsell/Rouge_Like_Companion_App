2293ffc7f5e748b2535729ee4a6b92073b79b3eb
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2022-03-02 23:12:17 -0500

    Added color changing background for sliding window sliceing

--- RoguelikeCompanion/NeuralNetworkVisualization/NN_Visualization.cs
+++ RoguelikeCompanion/NeuralNetworkVisualization/NN_Visualization.cs
@@ -9,7 +9,7 @@ namespace NeuralNetworkVisualization
 {
     public partial class NN_Visualization : Form
     {
-        // Fields
+        // Class Fields
         NeuralNetwork nn = new NeuralNetwork();
         string textBoxPathName = @"C:\Users\Grant\Desktop\Java_Rouge_Like_App\screenshots\temp_1.jpg";
         List<Bitmap> slidingWindowImages = new List<Bitmap>();
@@ -19,27 +19,32 @@ namespace NeuralNetworkVisualization
         Timer slideShowTimer = new Timer();
 
         /*
+         * NN_Visualization constructor.
          */
         public NN_Visualization()
         {
             InitializeComponent();
+
+            // Read in new image 
             Bitmap newImage = convertToBitmap(textBoxPathName);
-            showExtractedTextBox(newImage);
-            showCurrentSlidingWindow(newImage);
+
             string guess = nn.newImagePrediction(newImage);
-            //imageIsolation(newImage);
 
             // Obtain all sliding window images
             (slidingWindowImages, isCharacterList) = imageIsolation(newImage);
 
             // Image Slideshow via timer
-            slideShowTimer.Interval = (2000);
+            slideShowTimer.Interval = (500);
             slideShowTimer.Tick += new EventHandler(slideShow_Tick);
             slideShowTimer.Start();
         }
 
 
         /*
+         * Popuplates the 'extractedTextBox' image property.
+         * 
+         * @param textBoxImage, the image used to populate the image property.
+         * @return none.
          */
         public void showExtractedTextBox(Bitmap textBoxImage)
         {
@@ -50,6 +55,10 @@ namespace NeuralNetworkVisualization
 
 
         /*
+         * Popuplates the 'currentSlidingWindowSlice' image property.
+         * 
+         * @param textBoxImage, the image used to populate the image property.
+         * @return none.
          */
         public void showCurrentSlidingWindow(Bitmap slidingWindow)
         {
@@ -60,6 +69,10 @@ namespace NeuralNetworkVisualization
 
 
         /*
+         * Converts a png, jpg image to a bitmap.
+         * 
+         * @param fileName, the absolute file path of the picture location.
+         * @return a Bitmap, representing the converted image.
          */
         public Bitmap convertToBitmap(string fileName)
         {
@@ -74,20 +87,20 @@ namespace NeuralNetworkVisualization
 
 
         /*
+         * Obtains the sliding window bitmap slices and marks them as either a box to be
+         *   process or not.
+         *
+         * @params newImage, a bitmap denoting the image to be processed.
+         * @return two lists, one containing the sliding window slices, and one
+         *   containing if the respective bitmap is a character to be processed.
          */
         public (List<Bitmap>, List<bool>) imageIsolation(Bitmap newImage)
         {
-            // Timer
-            System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
-            int delayTime = 2000;
-            myTimer.Interval = delayTime;
-
             // Sliding window dimensions and step size
             int swHeight = 18, swWidth = 15, swDelta = 5;
 
             // Obtain indicies for character separation
             List<int> charSeparationIndicies;
-            
             (charSeparationIndicies, isolatedTextImage) = CharacterSegmentation.characterSegmentation(newImage);
 
             // Create List of Sliding Window Images
@@ -108,6 +121,14 @@ namespace NeuralNetworkVisualization
 
 
         /*
+         * Iterates, and shows the next sliding window slice in the slidingWindowImage
+         *   list.
+         * @param slidingWindowImages, a list of all of the sliding window slices.
+         * @param foundLetters, a list denoting if the respective sliding window slice is
+         *   a character that should be processed.
+         * @param image, the original image from which all of the sliding window slices 
+         *   were obtained.
+         * @return none.
          */
         public void imageSlideShow(List<Bitmap> slidingWindowImages, List<bool> foundLetters, Bitmap image)
         {
@@ -115,15 +136,25 @@ namespace NeuralNetworkVisualization
             int swHeight = 18, swWidth = 15, swDelta = 5;
 
             showExtractedTextBox(image);
-            Bitmap test = ScreenImgCapture.cropBitMap(image, currSWIndex, swWidth, 0, swHeight);
+            //Bitmap test = ScreenImgCapture.cropBitMap(image, currSWIndex, swWidth, 0, swHeight);
+            Bitmap test = slidingWindowImages[currSWIndex];
             showCurrentSlidingWindow(test);
+
+            // Change sliding window background color
+            if (foundLetters[currSWIndex])
+                slidingWindowBackground.BackColor = Color.Green;
+            else
+                slidingWindowBackground.BackColor = Color.Red;
+
             currSWIndex++;
-            if (currSWIndex > 8)
+            if (currSWIndex > image.Width - swWidth + swDelta)
                 slideShowTimer.Stop();
+            
         }
 
 
         /*
+         * Method to be run when the timer is raised. Calls the imageSlideShow method.
          */
         public void slideShow_Tick(object sender, EventArgs e)
         {

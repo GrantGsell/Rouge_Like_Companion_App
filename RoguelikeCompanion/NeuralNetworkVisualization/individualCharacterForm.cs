d711ef2bbe746053c2147dc7473db01281299215
GrantGsell <47372698+GrantGsell@users.noreply.github.com>
2022-03-04 22:55:59 -0500

    Fixed VS issue, trimmed code, changed DGV to TextBox

--- RoguelikeCompanion/NeuralNetworkVisualization/individualCharacterForm.cs
+++ RoguelikeCompanion/NeuralNetworkVisualization/individualCharacterForm.cs
@@ -1,27 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace RoguelikeCompanion
+namespace NeuralNetworkVisualization
 {
     public partial class individualCharacterForm : Form
     {
-        PictureBox synergyImage;
-        DataGridView synergyData;
+        PictureBox characterImage;
+        TextBox characterGuess = new TextBox();
 
         /*
-         * Constructor that creates the PictureBox and DataGridView controls
+         * Constructor that creates the PictureBox and teatbox controls
          * for this form.
          * 
-         * @param img, the image of the synergy object.
-         * @param synergizesWith, the name of the object held by the user, that
-         *      the synergy object synergizes with.
+         * @param img, the image of the extracted character
+         * @param synergizesWith, the guess for the character represente by the
+         *   bitmap image.
          */
-        public individualCharacterForm(Image img, string synergizesWith)
+        public individualCharacterForm(Bitmap img, string character)
         {
             InitializeComponent();
-            this.synergyData = synergyDataGrid(synergizesWith);
-            this.synergyImage = IndividualWeaponForm.createPictureBox(img, 75, 75);
+            this.characterGuess.Text = character;
+            this.characterImage = createPictureBox(img, 75, 75);
 
             // Remove borders, set double buffer
             this.FormBorderStyle = FormBorderStyle.None;
@@ -29,85 +35,92 @@ namespace RoguelikeCompanion
 
 
         /*
-         * Loads this form with the synergy objects image and a DGV specifying 
-         * what object it synergizes with.
+         * Loads this form with the character bitmap image and the prediction
+         *   for that character.
          */
-        private void IndividualSynergyForm_Load(object sender, EventArgs e)
+        private void individualCharacterForm_Load(object sender, EventArgs e)
         {
             // Create an image object in the top left corner
-            synergyImage.Location = new Point(0, 75 - synergyImage.Height);
+            characterImage.Location = new Point(0, 75 - characterImage.Height);
 
-            // Place the synergy DataGridView object below the image
-            int imageHeightOffset = synergyImage.Height;
-            synergyData.Location = new Point(0, (75 - synergyImage.Height) + imageHeightOffset);
+            // Place thee TextBox below the image
+            int imageHeightOffset = characterImage.Height;
+            characterGuess.Location = new Point(0, (75 - characterImage.Height) + imageHeightOffset);
 
             // Set form size
-            int height = synergyData.Height + 75;
-            int width = (synergyData.Width > synergyImage.Width) ? synergyData.Width : synergyImage.Width;
+            int height = characterGuess.Height + 75;
+            int width = (characterGuess.Width > characterImage.Width) ? characterGuess.Width : characterImage.Width;
             this.Size = new Size(width, height);
 
             // Center image
-            int updatedImageWidth = Math.Abs(synergyData.Width / 2 - synergyImage.Width / 2);
-            synergyImage.Location = new Point(updatedImageWidth, 75 - synergyImage.Height);
+            int updatedImageWidth = Math.Abs(characterGuess.Width / 2 - characterImage.Width / 2);
+            characterImage.Location = new Point(updatedImageWidth, 75 - characterImage.Height);
 
-            // Add image and data grid to the form
-            this.Controls.Add(synergyImage);
-            this.Controls.Add(synergyData);
+            // Add image and TextBox
+            this.Controls.Add(characterImage);
+            this.Controls.Add(characterGuess);
         }
 
 
         /*
-         * Creates a DataGridView control to contain the name of a single
-         * object that this synergy object synergizes with.
+         * Creates a new PictureBox control, scales the given image and then
+         * places it into the new control. The control is sized to fit the
+         * scaled image size and not the rectangle specified by newWidth,
+         * newHeight.
          * 
-         * @param synergizesWith, the name of the object that this synergy
-         *      object synergizes with. This object is held by the user.
-         * @return synergyGrid, a 2x1 DGV, Row 1 contains the text "Synergizes
-         *      With", Row 2 contains the name specified by synergizesWith.
+         * @param img, the image of the object.
+         * @param newWidth, the maximum width of the rectangle to scale the 
+         *      image into.
+         * @param newHeight, the maximum height of the rectangle to scale the
+         *      image into.
+         * @return imageBox, a PictureBox control containing the scaled image.
          */
-        public DataGridView synergyDataGrid(string synergizesWith)
+        public static PictureBox createPictureBox(Bitmap bm, int newWidth, int newHeight)
         {
-            // Remove \\ and _ from synergizesWith objet name
-            synergizesWith = synergizesWith.Replace("\\", "").Replace("_", " ");
+            PictureBox imageBox = new PictureBox();
 
-            // Create a new container to hold item data
-            DataGridView synergyGrid = new DataGridView();
+            // Scale bit map
+            bm = ScaleImage(bm, newHeight, newWidth);
 
-            // Set interactive options to off
-            synergyGrid.ReadOnly = true;
-            synergyGrid.AllowUserToOrderColumns = false;
-            synergyGrid.AllowUserToResizeRows = false;
-            synergyGrid.AllowUserToResizeColumns = false;
-            synergyGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            synergyGrid.ColumnHeadersDefaultCellStyle.WrapMode = DataGridViewTriState.False;
+            // Set the picture box size to fit the scaled image
+            imageBox.Height = bm.Height;
+            imageBox.Width = bm.Width;
+            imageBox.Image = bm;
 
-            // Turn 'off' cell highlighting
-            synergyGrid.DefaultCellStyle.SelectionBackColor = synergyGrid.DefaultCellStyle.BackColor;
-            synergyGrid.DefaultCellStyle.SelectionForeColor = synergyGrid.DefaultCellStyle.ForeColor;
-
-            // Turn off scrolling and row headers
-            synergyGrid.ScrollBars = ScrollBars.None;
-            synergyGrid.RowHeadersVisible = false;
-
-            // Set number of columns, names and center headers/column[0]
-            synergyGrid.ColumnCount = 1;
-            synergyGrid.Columns[0].Name = "Synergizes With:";
-            synergyGrid.Columns[0].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            synergyGrid.Columns[0].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            synergyGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-
-            // Turn off sorting for columns
-            synergyGrid.Columns[0].SortMode = DataGridViewColumnSortMode.NotSortable;
-
-            // Populate 1 row
-            synergyGrid.RowCount = 1;
-            synergyGrid.Rows[0].SetValues(new string[] { synergizesWith });
-
-            // Set DataGridView size
-            int height = synergyGrid.ColumnHeadersHeight + synergyGrid.Rows[0].Height;
-            synergyGrid.Size = new Size( synergyGrid.Width, height);
-
-            return synergyGrid;
+            return imageBox;
         }
+
+
+        /*
+         * Transforms an image from its original size into the maximum size
+         * that allows the image to fit in a give rectangle of maxWidth x 
+         * maxHeight.
+         * 
+         * @param bmp, the original image.
+         * @param maxWidth, the maximum width of the rectangle to scale the
+         *      image to fit into.
+         * @param maxHeight, the maximum height of the rectangle to scale the
+         *      image to fit into.
+         * @return newImage, the original image scaled to fit into a rectangle
+         *      given by the input parameters.
+         */
+        public static Bitmap ScaleImage(Bitmap bmp, int maxWidth, int maxHeight)
+        {
+            var ratioX = (double)maxWidth / bmp.Width;
+            var ratioY = (double)maxHeight / bmp.Height;
+            var ratio = Math.Min(ratioX, ratioY);
+
+            var newWidth = (int)(bmp.Width * ratio);
+            var newHeight = (int)(bmp.Height * ratio);
+
+            var newImage = new Bitmap(newWidth, newHeight);
+
+            using (var graphics = Graphics.FromImage(newImage))
+                graphics.DrawImage(bmp, 0, 0, newWidth, newHeight);
+
+            return newImage;
+        }
+
     }
+
 }
